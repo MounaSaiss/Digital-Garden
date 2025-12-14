c4e226f204050f397f97c7c208e3f6ef35df5825
MounaSaiss <mouna1.saiss@gmail.com>
2025-12-14 13:28:29 +0100

    refactor:add nav and headre in the same file nav and includes in index page

--- digital-garden/index.php
+++ digital-garden/index.php
@@ -1,4 +1,3 @@
-<?php require_once __DIR__ . '/includes/header.php'; ?>
 <?php require_once __DIR__ . '/includes/navbar.php'; ?>
 
 
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,17 @@
-<nav class="bg-[#173B2D] text-white">
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digital Garden</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="/public/fichier.css">
+    <script src="https://kit.fontawesome.com/a680a19743.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <nav class="bg-[#173B2D] text-white">
         <div class="container mx-auto px-6 lg:px-20">
             <div class="flex items-center justify-between h-16">
                 <div class="text-xl font-semibold">
