3af530881713fa154d02b8914133f836365c284a
MounaSaiss <mouna1.saiss@gmail.com>
2025-12-15 22:48:48 +0100

    feat:create database schema table users,theme and note

---
+++ sql/schema.sql
@@ -0,0 +1,26 @@
+
+USE garden;
+CREATE TABLE  users (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    dateInscription DATE NOT NULL
+);
+CREATE TABLE theme(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(25) NOT NULL,
+    badgeCouleur VARCHAR(30) NOT NULL,
+    tags VARCHAR(30),
+    id_user INT NOT NULL,
+    FOREIGN KEY (id_user)REFERENCES users(id)
+); 
+CREATE TABLE note (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(50) NOT NULL,
+    importance INT NOT NULL CHECK(importance BETWEEN 1 AND 5),
+    contenu VARCHAR(255) NOT NULL,
+    dateCreation DATE NOT NULL,
+    id_theme INT NOT NULL ,
+    FOREIGN KEY(id_theme) REFERENCES theme(id)
+);
+
